da1ea0a5a126e1efbfd7723b8ad88b1d9b896fb1
jakemayo254 <jakemayo254@gmail.com>
2025-03-19 12:17:45 -0500

    able to add, delete and get orders

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { PizzaService } from '../services/pizza.service';
 import { AuthDetails } from '../models/auth.model';
 import { FormsModule } from '@angular/forms';
 import {CommonModule} from '@angular/common';
+import {Order} from '@src/app/models/order.model';
 
 @Component({
   selector: 'app-login',
@@ -15,9 +16,17 @@ export class LoginComponent {
     password: ''
   }
   authToken = '';
-
+  deleteOrderID = null;
   showPassword = false;
   errorMessage = '';
+  orders: Order[] = []
+
+  newOrderTableNo: number | null = null;
+  newOrderCrust = null;
+  newOrderFlavor = null;
+  newOrderSize = null;
+
+  newOrder: Order | null = null;
 
   constructor(private authService: PizzaService) {}
 
@@ -25,11 +34,45 @@ export class LoginComponent {
     this.showPassword = !this.showPassword;
   }
 
+  submitOrder(): void {
+    if (this.newOrderTableNo != null && this.newOrderFlavor != null && this.newOrderCrust != null && this.newOrderSize != null) {
+      let newOrder: Order = {
+        Table_No: this.newOrderTableNo,
+        Flavor: this.newOrderFlavor,
+        Crust: this.newOrderCrust,
+        Size: this.newOrderSize
+      }
+
+      this.authService.createOrder(newOrder, this.authToken).subscribe(res => {
+        console.log("New Order Details: " + JSON.stringify(res));
+        this.newOrder = res;
+      })
+    }
+  }
+
+
+  getOrders(): void {
+    this.authService.getAllOrders().subscribe(orders => this.orders = orders);
+  }
+
+  deleteOrder(): void {
+    if (this.deleteOrderID != null) {
+      this.authService.deleteOrder(this.deleteOrderID).subscribe({
+        next: result => {
+          console.log("delete result: " + JSON.stringify(result));
+        }
+      })
+
+      this.getOrders()
+    }
+  }
+
   onSubmit(): void {
     this.authService.getAuthToken(this.authDetails).subscribe({
       next: (res) => {
         console.log("Access Token: " + res.access_token)
-        localStorage.setItem('access_token', res.access_token);
+        this.authToken = res.access_token;
+        //localStorage.setItem('access_token', res.access_token);
         // Redirect or show success
       },
       error: (err) => {
--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -1,8 +1,8 @@
 export interface Order {
     Crust: string;
     Flavor: string;
-    Order_ID: number;
+    Order_ID?: number;
     Size: string;
     Table_No: number;
-    Timestamp: Date;
+    Timestamp?: Date;
   }
